d67c6757d0dff17a16046958bd193d84ccaba080
petark7 <kuzmanovski.petar@uklo.edu.mk>
2022-11-01 16:34:35 +0100

    Implement grid size button
    + improve code

--- code.js
+++ code.js
@@ -1,18 +1,46 @@
 let grid = document.getElementsByClassName("grid")[0];
 let gridElements = [];
 let numberOfRows = 16;
+let toggledRainbow = false;
 
-grid.style.cssText = (`grid-template-columns: repeat(${numberOfRows}, auto);`);
+let btnToggle = document.getElementById('toggleRainbow');
+let btnChangeGridSize = document.getElementById('changeGridSize');
 
-for (let i = 0; i < numberOfRows * numberOfRows; i++)
+// render grid
+renderGrid(numberOfRows);
+
+btnChangeGridSize.addEventListener('click', function() {
+    while (true)
+    {
+        let userInput = prompt ('Enter a value up to 100');
+        if (userInput < 100) {
+            grid.innerHTML = '';
+            gridElements = [];
+            renderGrid(userInput);
+            break;
+        }
+    }
+
+})
+
+function renderGrid (numberOfRows) {
+    grid.style.cssText = (`grid-template-columns: repeat(${numberOfRows}, auto);`);
+    for (let i = 0; i < numberOfRows * numberOfRows; i++)
 {
     gridElements[i] = document.createElement("div");
     gridElements[i].setAttribute('class','gridElement');
     gridElements[i].style.cssText = ('border: 1px solid;');
     gridElements[i].addEventListener('mouseover', function onMouseOver () {
-        gridElements[i].style.cssText = (`background-color: rgb${randomColor()};`);
+        if (toggledRainbow)
+        {
+            gridElements[i].style.cssText = (`background-color: rgb${randomColor()};`);
+        }
+        else {
+            gridElements[i].style.cssText = (`background-color: black`);
+        }
     });
     grid.appendChild(gridElements[i]);
+}
 }
 
 // returns random rgb value in the format (r, g, b)
