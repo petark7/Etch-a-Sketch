7d4406d4cd63ac44f3d45aaa7b4bea1dc9f6ea2c
petark7 <kuzmanovski.petar@uklo.edu.mk>
2022-11-01 16:02:23 +0100

    Implement random color

--- code.js
+++ code.js
@@ -9,6 +9,13 @@ for (let i = 0; i < numberOfRows * numberOfRows; i++)
     gridElements[i] = document.createElement("div");
     gridElements[i].setAttribute('class','gridElement');
     gridElements[i].style.cssText = ('border: 1px solid;');
+    gridElements[i].addEventListener('mouseover', function onMouseOver () {
+        gridElements[i].style.cssText = (`background-color: rgb${randomColor()};`);
+    });
     grid.appendChild(gridElements[i]);
 }
 
+// returns random rgb value in the format (r, g, b)
+function randomColor () {
+    return `(${Math.floor (Math.random() * 255)}, ${Math.floor (Math.random() * 255)}, ${Math.floor (Math.random() * 255)})`;
+}
